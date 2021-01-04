af888c9fec710be0785ab8a49afd0e0922623f85
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-04 11:36:44 +0200

    Ne pregătim să putem adăuga oscilații noi

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, ListGroup, Row } from 'react-bootstrap';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import randomColor from 'randomcolor';
 
 import Graph from './Graph';
 
+interface Osci {
+  color: string,
+  amplitudine: number,
+  pulsatie: number,
+  fazaInitiala: number
+};
+
 function App() {
+  const [ oscilatii, setOscilatii ] = useState<Osci[]>([])
+  function addOsci() {
+    setOscilatii([ ...oscilatii, {
+      color: randomColor(),
+      amplitudine: 10,
+      pulsatie: 1,
+      fazaInitiala: 0
+    }]);
+  };
   return (
     <Container>
+      <Row><h1>Oscilații de compus</h1></Row>
+      <Row xs={12}><ListGroup>
+        {oscilatii.map((oscilatie) => {
+          return <ListGroup.Item>{oscilatie.color}</ListGroup.Item>
+        })}
+        <ListGroup.Item action active onClick={addOsci}><BsPlusCircleFill /> Adaugă oscilație nouă</ListGroup.Item>
+      </ListGroup></Row>
       <Row><h1>Oscilație rezultantă</h1></Row>
-      <Row >
-        <Graph fn={(a: number) => 200*Math.sin(a/10)} height={200} color="#ffffff" id="1"/>
+      <Row xs={12}>
+        <Graph fn={(a) => 100*Math.sin(a/10)} height={200} color="#ffffff" />
       </Row>
     </Container>
   );
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import SmoothieComponent, { TimeSeries } from 'react-smoothie';
 
 interface Props {
-    fn: (val: number) => number;
-    id: string;
-    height: number;
-    color: string;
+  fn: (val: number) => number;
+  height: number;
+  color: string;
 }
 
 function Graph(props: Props) {
-    var i=0;
-    var line = new TimeSeries();
-    setInterval(() => {
-        line.append(Date.now(), props.fn(i++))
-    }, 40);
-    return (
-        <SmoothieComponent maxValue={props.height/2} minValue={-props.height/2} series={[{
-            data: line,
-            strokeStyle: props.color,
-            lineWidth: 4,
-        }]}/>
-    );
+  var i=0;
+  var line = new TimeSeries();
+  setInterval(() => {
+    line.append(Date.now(), props.fn(i++))
+  }, 40);
+  return (
+   <SmoothieComponent responsive maxValue={props.height/2} minValue={-props.height/2} series={[{
+     data: line,
+     strokeStyle: props.color,
+     lineWidth: 4,
+   }]}/>
+  );
 }
 
 export default Graph;
