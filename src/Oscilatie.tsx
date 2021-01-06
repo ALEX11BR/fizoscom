c48b721dd994e0fb0aab09880ba437b637f575d2
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-06 03:34:27 +0200

    Adăugat câmpuri și buton de ștergere la componenta Oscilatie

--- src/Oscilatie.tsx
+++ src/Oscilatie.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-
+import { TableRow, TableCell, Input, InputAdornment, IconButton } from '@material-ui/core';
+import { DeleteForever } from '@material-ui/icons'
 import Graph from './Graph';
 
 interface Props {
@@ -13,15 +12,27 @@ function Oscilatie(props: Props) {
   return (
     <TableRow>
         <TableCell>
-          <b>{props.index+1}.</b>
+          <b>{props.index+1}.</b><IconButton size="small"><DeleteForever /></IconButton>
         </TableCell>
         <TableCell>
           <Graph color={props.osci.color} height={100} fn={(a) => 50*Math.sin(a/10+5)} />
         </TableCell>
         <TableCell>
-          <p>Amplitudine&nbsp;(A):</p>
-          <p>Pulsație&nbsp;(ω):</p>
-          <p>Faza&nbsp;inițială&nbsp;(φ<sub>0</sub>):</p>
+          <p>Amplitudine&nbsp;(A):
+            <Input value={props.osci.amplitudine} endAdornment={<InputAdornment position="end">
+              px
+            </InputAdornment>} />
+          </p>
+          <p>Pulsație&nbsp;(ω):
+            <Input value={props.osci.pulsatie} endAdornment={<InputAdornment position="end">
+              rad/s
+            </InputAdornment>} />
+          </p>
+          <p>Faza&nbsp;inițială&nbsp;(φ<sub>0</sub>):
+            <Input value={props.osci.fazaInitiala} endAdornment={<InputAdornment position="end">
+              rad
+            </InputAdornment>} />
+          </p>
         </TableCell>
     </TableRow>
   );
