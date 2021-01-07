8968861e7dcaf8b6a6d9475762e88a31748daa3a
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-07 15:47:36 +0200

    Adăugat worker-timers ca graficele să funcționeze chiar și pe fundal fără probleme

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SmoothieComponent, { TimeSeries } from 'react-smoothie';
-
+import * as workerTimers from 'worker-timers';
 interface Props {
   fn: (val: number) => number,
   height: number,
@@ -10,7 +10,7 @@ interface Props {
 function Graph(props: Props) {
   var i=0;
   var line = new TimeSeries();
-  setInterval(() => {
+  workerTimers.setInterval(() => {
     line.append(Date.now(), props.fn(i++))
   }, 40);
   return (
