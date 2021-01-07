4e4e6e0c345589504892705f671ec4003e4ab8c3
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-07 04:24:53 +0200

    Adăugat câmpuri cu valori de setat parametrii oscilației

--- src/Oscilatie.tsx
+++ src/Oscilatie.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Row, Col, List, Button, Tooltip } from 'antd';
+import React, { useState } from 'react';
+import { Row, Col, List, Button, Tooltip, InputNumber } from 'antd';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import Graph from './Graph';
@@ -11,23 +11,26 @@ interface Props {
 }
 
 function Oscilatie(props: Props) {
+  const [ amplitudine, setAmplitudine ] = useState(props.osci.amplitudine);
+  const [ pulsatie, setPulsatie ] = useState(props.osci.pulsatie);
+  const [ fazaInitiala, setFazaInitiala ] = useState(props.osci.fazaInitiala);
   return (
     <List.Item>
-      <Row>
-        <Col xs={1}>
+      <Row gutter={16} align="middle">
+        <Col xs={2}>
           <Tooltip title="Șterge oscilația">
             <Button onClick={props.onDelete} danger type="primary" shape="circle" icon={
               <AiOutlineClose />
             }/>
           </Tooltip>
         </Col>
-        <Col>
+        <Col xs={16}>
           <Graph color={props.osci.color} height={100} fn={(a) => 50*Math.sin(a/10+5)} />
         </Col>
-        <Col>
-          <Row><span>Amplitudine (A):</span></Row>
-          <Row><span>Pulsație (ω):</span></Row>
-          <Row><span>Faza inițială (φ<sub>0</sub>):</span></Row>
+        <Col xs={6}>
+          <div>Amplitudine (px):<br /><InputNumber defaultValue={amplitudine} onChange={n => setAmplitudine(Number(n))} /></div>
+          <div>Pulsație (rad/s):<br /><InputNumber  defaultValue={pulsatie} onChange={n => setPulsatie(Number(n))} /></div>
+          <div>Faza inițială (rad):<br /><InputNumber defaultValue={fazaInitiala} onChange={n => setFazaInitiala(Number(n))} /></div>
         </Col>
       </Row>
     </List.Item>
