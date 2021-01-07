d73c7651270be3beea5effa8c81fb2238dbbf9c8
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-07 09:20:33 +0200

    Adăugat câmpuri de valori la componenta Oscilatie

--- src/Oscilatie.tsx
+++ src/Oscilatie.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ListGroupItem, Col, Row, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { ListGroupItem, Col, Row, Button, InputGroup, FormControl } from 'react-bootstrap';
 import { BsTrashFill } from 'react-icons/bs';
 
 import Graph from './Graph';
@@ -11,20 +11,44 @@ interface Props {
 }
 
 function Oscilatie(props: Props) {
+  const [ amplitudine, setAmplitudine ] = useState(props.osci.amplitudine);
+  const [ pulsatie, setPulsatie ] = useState(props.osci.pulsatie);
+  const [ fazaInitiala, setFazaInitiala ] = useState(props.osci.fazaInitiala);
   return (
     <ListGroupItem>
       <Row>
         <Col xs={1}>
-          <b>{props.index+1}.</b>
+          <b>{props.index+1}.</b><br />
           <Button variant="danger" size="sm" onClick={props.onDelete}><BsTrashFill /></Button>
         </Col>
         <Col>
           <Graph color={props.osci.color} height={100} fn={(a) => 50*Math.sin(a/10+5)} />
         </Col>
         <Col>
-          <Row><span>Amplitudine (A):</span></Row>
-          <Row><span>Pulsație (ω):</span></Row>
-          <Row><span>Faza inițială (φ<sub>0</sub>):</span></Row>
+          <Row><span>Amplitudine (A):</span>
+            <InputGroup>
+              <FormControl type="number" value={amplitudine} onChange={e => setAmplitudine(parseFloat(e.target.value))}></FormControl>
+              <InputGroup.Append>
+                <InputGroup.Text>px</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+          <Row><span>Pulsație (ω):</span>
+            <InputGroup>
+              <FormControl type="number" value={pulsatie} onChange={e => setPulsatie(parseFloat(e.target.value))}></FormControl>
+              <InputGroup.Append>
+                <InputGroup.Text>rad/s</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+          <Row><span>Faza inițială (φ<sub>0</sub>):</span>
+            <InputGroup>
+              <FormControl type="number" value={fazaInitiala} onChange={e => setFazaInitiala(parseFloat(e.target.value))}></FormControl>
+              <InputGroup.Append>
+                <InputGroup.Text>rad</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
         </Col>
       </Row>
     </ListGroupItem>
