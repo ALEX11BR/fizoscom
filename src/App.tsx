3a6f016479efa2b7731123765355e9ab299cdc3d
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-01-07 02:47:51 +0200

    Îmbunătățiri ale interfaței

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Row, Col, Card } from 'antd';
-import { BsPlusCircleFill } from 'react-icons/bs';
+import { Row, Col, Card, List, Button, Tooltip } from 'antd';
+import { AiOutlinePlus } from 'react-icons/ai';
 import randomColor from 'randomcolor';
 
 import Graph from './Graph';
@@ -16,19 +16,36 @@ function App() {
       fazaInitiala: 0
     }]);
   };
+  function deleteOsci(index: number) {
+    setOscilatii([
+      ...oscilatii.slice(0,index),
+      ...oscilatii.slice(index+1)
+    ]);
+  };
   return (
     <Row>
-      <Col>
-        <Card title="Oscilații de compus" extra={
-          <a href="#" onClick={addOsci}>Adaugă</a>
-        }>
-          {oscilatii.map((oscilatie, index) => {
-            return <Oscilatie osci={oscilatie} index={index}/>
-          })}
+      <Col xs={24} lg={12}>
+        <Card>
+          <Row justify="space-between">
+            <Col><h2>Oscilații de compus</h2></Col>
+            <Col>
+              <Tooltip title="Adaugă oscilație">
+                <Button onClick={addOsci} type="primary" shape="circle" icon={
+                  <AiOutlinePlus />
+                }/>
+              </Tooltip>
+            </Col>
+          </Row>
+          <List bordered>
+            {oscilatii.map((oscilatie, index) => {
+              return <Oscilatie osci={oscilatie} index={index} onDelete={() => deleteOsci(index)}/>
+            })}
+          </List>
         </Card>
       </Col>
-      <Col>
-        <Card title="Oscilație rezultantă">
+      <Col xs={24} lg={12}>
+        <Card>
+          <h2>Oscilație rezultantă</h2>
           <Graph fn={(a) => 100*Math.sin(a/10)} height={200} color="#ffffff" />
         </Card>
       </Col>
--- src/Oscilatie.tsx
+++ src/Oscilatie.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, List, Button, Tooltip } from 'antd';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import Graph from './Graph';
 
 interface Props {
   osci: Osci,
   index: number
+  onDelete: () => void
 }
 
 function Oscilatie(props: Props) {
   return (
-    <Row>
-      <Col xs={1}>
-        <b>{props.index+1}.</b>
-      </Col>
-      <Col>
-        <Graph color={props.osci.color} height={100} fn={(a) => 50*Math.sin(a/10+5)} />
-      </Col>
-      <Col>
-        <Row><span>Amplitudine (A):</span></Row>
-        <Row><span>Pulsație (ω):</span></Row>
-        <Row><span>Faza inițială (φ<sub>0</sub>):</span></Row>
-      </Col>
-    </Row>
+    <List.Item>
+      <Row>
+        <Col xs={1}>
+          <Tooltip title="Șterge oscilația">
+            <Button onClick={props.onDelete} danger type="primary" shape="circle" icon={
+              <AiOutlineClose />
+            }/>
+          </Tooltip>
+        </Col>
+        <Col>
+          <Graph color={props.osci.color} height={100} fn={(a) => 50*Math.sin(a/10+5)} />
+        </Col>
+        <Col>
+          <Row><span>Amplitudine (A):</span></Row>
+          <Row><span>Pulsație (ω):</span></Row>
+          <Row><span>Faza inițială (φ<sub>0</sub>):</span></Row>
+        </Col>
+      </Row>
+    </List.Item>
   );
 }
 
